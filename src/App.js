c7bec46e6d0c01451272c4b614308364152bf525
liggia <liggia.cruz@gmail.com>
2021-12-17 08:38:21 -0300

    preparing for deploy

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import './styles/reset.css';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import UserContext from './context/UserContext';
 
 export default function App() {
 
   return (
-  <UserContext.Provider>
     <Router>
       <Routes>
         <Route path = "/" exact element = {<Home />} />
       </Routes>
     </Router>
-  </UserContext.Provider>
   );
 }
